5a8bec8cfa9a5b392ea283569524319e40a3acf3
marco <marco@joinder.me>
2025-01-23 19:47:48 -0300

    Completly developed players area

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,35 @@
-export default function Player({ name, symbol }) {
+import React, { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // Best way of updating state based on the previous state
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} /> //Two way binding
+    );
+    btnCaption = "Save";
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
