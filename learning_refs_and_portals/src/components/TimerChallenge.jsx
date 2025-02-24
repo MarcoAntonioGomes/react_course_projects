87f7f5d8dd1d5a992eb219a95006644287efd5e1
marco <marco@joinder.me>
2025-02-24 19:57:06 -0300

    using refs in a case where the UI does not need to be reload.

--- learning_refs_and_portals/src/components/TimerChallenge.jsx
+++ learning_refs_and_portals/src/components/TimerChallenge.jsx
@@ -1,14 +1,36 @@
+import { useRef, useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} seconds{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p>Time is running... / Timer inactive</p>
+      <p>{timerStarted ? "Time is running..." : "Timer inactive"}</p>
     </section>
   );
 }
