6150a132cd5ee0d1265a29a430e1de06db8ca92f
marco <marco@joinder.me>
2025-04-30 13:48:35 -0300

    Learned useMemo to solve function unecessary execution and improve perfomance

--- react_advanced_behind_the_scenes/src/components/Counter/Counter.jsx
+++ react_advanced_behind_the_scenes/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useState } from "react";
+import { memo, useCallback, useMemo, useState } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -26,7 +26,11 @@ function isPrime(number) {
 // memo is used to prevent re-rendering the component when the props are the same
 const Counter = memo(function Counter({ initialCount }) {
   log("<Counter /> rendered", 1);
-  const initialCountIsPrime = isPrime(initialCount);
+
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
 
   const [counter, setCounter] = useState(initialCount);
 
