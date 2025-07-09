3e9ecee8058b0e9bc5c7c9a25563432888483cf5
marco <marco@joinder.me>
2025-07-08 21:23:08 -0300

    Fetching data from a data base with next js

--- nextjs-foodies-app/app/meals/page.js
+++ nextjs-foodies-app/app/meals/page.js
@@ -1,14 +1,28 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
-      <div>MealsPage</div>
-      <div>
-        <Link href="/meals/meal-1">Meal 1</Link>
-        <Link href="/meals/meal-2">Meal 2</Link>
-        <Link href="/meals/meal-3">Meal 3</Link>
-      </div>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
   );
 }
